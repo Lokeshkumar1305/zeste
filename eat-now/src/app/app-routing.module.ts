9a6ea358cbe3844c95e6dedeeaa5279d63e219a2
aravind.jilla <aravind.jilla@toucanus.com>
2025-02-24 23:01:21 +0530

    project set-up

---
+++ eat-now/src/app/app-routing.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
+
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  // { path: 'Home', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'core', loadChildren: () => import('./core/core.module').then(m => m.CoreModule), canActivate: [authGuard] },
+  { path: 'common', loadChildren: () => import('./common-library/common-library.module').then(m => m.CommonLibraryModule), canActivate: [authGuard] },
+  { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: '**', redirectTo: '/auth/login', pathMatch: 'full' },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {
+
+}
---
+++ eat-now/src/app/common-library/common-library.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CommonLibraryRoutingModule } from './common-library-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CommonLibraryRoutingModule
+  ]
+})
+export class CommonLibraryModule { }
