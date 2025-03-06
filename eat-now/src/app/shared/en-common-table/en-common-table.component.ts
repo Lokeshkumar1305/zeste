1f280ff8b6f3f5d72da1b56583a9864e8b92591b
Lokesh-TC <lokesh.kanuboina@toucanus.com>
2025-03-07 00:05:03 +0530

    added common table service

--- eat-now/src/app/shared/en-common-table/en-common-table.component.ts
+++ eat-now/src/app/shared/en-common-table/en-common-table.component.ts
@@ -1,10 +1,384 @@
-import { Component } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { APIPath } from '../../common-library/api-enum';
+import { Subscription } from 'rxjs';
+import { MatSort } from '@angular/material/sort';
+import { EnTableSearchviewService } from '../services/en-table-searchview.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiService } from '../../common-library/services/api.service';
+import { MatDialog } from '@angular/material/dialog';
+import { LocaldataService } from '../services/localdata.service';
 
 @Component({
   selector: 'app-en-common-table',
   templateUrl: './en-common-table.component.html',
-  styleUrl: './en-common-table.component.scss'
+  styleUrl: './en-common-table.component.scss',
+  animations: [
+    trigger('searchExpand', [
+      state('collapsed', style({
+        width: '0px',
+        opacity: 0,
+      })),
+      state('expanded', style({
+        width: '*',
+        opacity: 1,
+      })),
+      transition('collapsed <=> expanded', [
+        animate('300ms ease-in-out')
+      ]),
+    ]),
+  ],
 })
 export class EnCommonTableComponent {
+  ctFields: any = [];
+  breadCrumb = new Array<OPSMenu>();
+  columnName: any;
+  headTitle = '';
+  subTitle = '';
+  pageSize: number = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+  pageIndex!: number;
+  // MatPaginator Output
+  pageEvent!: PageEvent;
+  pageNumber = 0;
+  totalElements = 0;
+  displayedColumns!: string[];
+  dataSource!: MatTableDataSource<any>;
+  modelKey!: string;
+  messageId!: APIPath;
+  @Input() tableID!: string;
+  private routeSubscription!: Subscription;
+  isSearch!: boolean;
+  value: any;
+  @ViewChild(MatPaginator)
+  paginator!: MatPaginator;
+  @ViewChild('commonTableSort') commonTableSort!: MatSort;
+  @ViewChild('commonTablePaginator') commonTablePaginator!: MatPaginator;
+  getPagination(event: any) {
 
+  }
+  constructor(public tableService: EnTableSearchviewService, private route: ActivatedRoute,
+    private apiService: ApiService, private router: Router, public dialog: MatDialog,
+    public localDataService: LocaldataService,
+
+  ) {
+    this.route.params.subscribe(routeParams => {
+      this.tableID = routeParams?.['id'];
+    });
+  }
+  ngOnInit() {
+    this.routeSubscription = this.route.params.subscribe(routeParams => {
+      if (routeParams['id']) {
+        this.tableID = routeParams['id'];
+      }
+      this.getTable();
+    });
+    // const bc = [
+    //   { "name": 'Home', "link": "/home" },
+    //   { "name": 'Disputes', "link": "/clearing&Settlement/disputes" },
+    // ];
+    // this.breadCrumb = bc;
+  }
+  searchField!: string;
+  getTable() {
+    this.headTitle = this.tableService.getHeadTitle(this.tableID);
+    this.subTitle = this.tableService.getSubHeadTitle(this.tableID);
+    const columnsData = this.tableService.getColumnName(this.tableID);
+    this.displayedColumns = columnsData.displayedColumns;
+    this.columnName = columnsData.columnNames;
+    this.messageId = this.tableService.getAPIPATH(this.tableID);
+    this.getAll(this.pageNumber, this.pageSize);
+  }
+  getAll(pageNumber: number, pageSize: number) {
+    let data;
+    this.localDataService.getDBEnum().subscribe((data: any) => {
+      // Extract data for the specified table name
+      const tableData = data[this.tableID] || [];
+      // Assign the filtered data to the dataSource
+      this.dataSource = new MatTableDataSource(tableData);
+      this.dataSource.sort = this.commonTableSort;
+      this.dataSource.paginator = this.commonTablePaginator;
+      // Optionally, set total elements based on the filtered data
+      // this.totalElements = this.dataSource.data.length;
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.routeSubscription) {
+      this.routeSubscription.unsubscribe();
+    }
+  }
+  search() {
+
+  }
+  // ngDoCheck() {
+  //   this.langCall();
+  // }
+  // langCall() {
+  //   let columnsData: { displayedColumns: string[]; columnNames: { [key: string]: string } } | null = null;
+  //   let lang = this.langService.localeLanguage;
+  //   switch (lang) {
+  //     case 'hi':
+  //       this.headTitle = this.langService.getHeadTitleinHindi(this.tableID);
+  //       this.subTitle = this.langService.getSubDescriptioninHindi(this.tableID);
+  //       columnsData = this.langService.getCTColumnNameinHindi(this.tableID);
+  //       break;
+  //     case 'es':
+  //       this.headTitle = this.langService.getHeadTitleinSpanish(this.tableID);
+  //       this.subTitle = this.langService.getSubDescriptioninSpanish(this.tableID);
+  //       columnsData = this.langService.getCTColumnNameinSpanish(this.tableID);
+  //       break;
+  //     case 'zh':
+  //       this.headTitle = this.langService.getHeadTitleinChinese(this.tableID);
+  //       this.subTitle = this.langService.getSubDescriptioninChinese(this.tableID);
+  //       columnsData = this.langService.getCTColumnNameinChinese(this.tableID);
+  //       break;
+  //     case 'ar':
+  //       this.headTitle = this.langService.getHeadTitleinArabic(this.tableID);
+  //       this.subTitle = this.langService.getSubDescriptioninArabic(this.tableID);
+  //       columnsData = this.langService.getCTColumnNameinArabic(this.tableID);
+  //       break;
+  //     default:
+  //       this.headTitle = this.tableService.getHeadTitle(this.tableID);
+  //       columnsData = this.tableService.getColumnName(this.tableID);
+  //       break;
+  //   }
+  //   if (columnsData) {
+  //     this.displayedColumns = columnsData.displayedColumns;
+  //     this.columnName = columnsData.columnNames;
+  //   } else {
+  //     console.warn('No valid columns data available');
+  //   }
+  // }
+
+
+
+
+
+
+
+  // newRow(tableID: string) {
+  //   let dialogRef!: any;
+  //   debugger
+  //   if (tableID == 'GET_SWITCH_DESTINATIONS') {
+  //     return this.tableService.goToCreate(tableID);
+  //   } else if (tableID == 'GET_ROUTING_RULES') {
+  //     dialogRef = this.dialog.open(RoutingRulesComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_SCHEME_CONNECTION') {
+  //     dialogRef = this.dialog.open(SchemeConnectionComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_BIN_DETAILS') {
+  //     dialogRef = this.dialog.open(BinTableComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_USERS') {
+  //     dialogRef = this.dialog.open(UserComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_ROLES') {
+  //     dialogRef = this.dialog.open(RoleComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_PRIVILEGES') {
+  //     dialogRef = this.dialog.open(PrivilegeComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_SERVICE_DIRECTORY') {
+  //     dialogRef = this.dialog.open(ServiceDirectoryComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //     });
+  //   } else if (tableID == 'GET_MAKER_CHECKER') {
+  //     return this.tableService.goToCreate(tableID);
+  //   }
+  //   if (tableID != 'GET_SWITCH_DESTINATIONS' && tableID != 'GET_MAKER_CHECKER') {
+  //     dialogRef.afterClosed().subscribe((result: any) => {
+  //       if (result) {
+  //         // this.logout();
+  //       }
+  //     });
+  //   }
+  // }
+
+
+  // editRow(tableID: string, dataObj: any) {
+  //   let dialogRef!: any;
+  //   if (tableID == 'GET_SWITCH_DESTINATIONS') {
+  //     dialogRef = this.dialog.open(SwitchDestinationComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_ROUTING_RULES') {
+  //     dialogRef = this.dialog.open(RoutingRulesComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_SCHEME_CONNECTION') {
+  //     dialogRef = this.dialog.open(SchemeConnectionComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_BIN_DETAILS') {
+  //     dialogRef = this.dialog.open(BinTableComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_USERS') {
+  //     dialogRef = this.dialog.open(UserComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_ROLES') {
+  //     dialogRef = this.dialog.open(RoleComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_PRIVILEGES') {
+  //     dialogRef = this.dialog.open(PrivilegeComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   } else if (tableID == 'GET_SERVICE_DIRECTORY') {
+  //     dialogRef = this.dialog.open(ServiceDirectoryComponent, {
+  //       width: '80%',
+  //       height: '700px',
+  //       position: { top: '0px', right: '0px' },
+  //       autoFocus: false,
+  //       minHeight: '100vh',
+  //       maxWidth: '80vw',
+  //       minWidth: '600px',
+  //       panelClass: 'custom-dialog-animation',
+  //       disableClose: true,
+  //       data: dataObj
+  //     });
+  //   }
+  //   if (tableID != 'GET_SWITCH_DESTINATIONS') {
+  //     dialogRef.afterClosed().subscribe((result: any) => {
+  //       if (result) {
+  //         // this.logout();
+  //       }
+  //     });
+  //   }
+  // }
+  applyFilter(event: any) {
+
+  }
+
+
+
+
+}
+
+export class OPSMenu {
+  name!: string;
+  link!: string;
 }
--- eat-now/src/app/shared/services/en-table-searchview.service.ts
+++ eat-now/src/app/shared/services/en-table-searchview.service.ts
@@ -1,9 +1,364 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../../common-library/services/api.service';
+import { APIPath } from '../../common-library/api-enum';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EnTableSearchviewService {
+  searchText!: string;
+  constructor(private router: Router, public apiService: ApiService,) { }
 
-  constructor() { }
+  public getHeadTitle(tableID: string) {
+    let returnValue = '';
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        returnValue = 'Scheme Connection';
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        returnValue = 'Switch Destination';
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        returnValue = 'Transaction Details';
+        break;
+      case 'GET_BIN_DETAILS':
+        returnValue = 'Bin Details';
+        break;
+      case 'GET_USERS':
+        returnValue = 'Users';
+        break;
+      case 'GET_ROUTING_RULES':
+        returnValue = 'Routing Table';
+        break;
+      case 'GET_ROLES':
+        returnValue = 'Roles';
+        break;
+      case 'GET_PRIVILEGES':
+        returnValue = 'Privileges';
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        returnValue = 'Service Directory';
+        break;
+      case 'GET_MAKER_CHECKER':
+        returnValue = 'Maker Checker';
+        break;
+      default:
+        break;
+    }
+    return returnValue;
+  }
+  public getSubHeadTitle(tableID: string) {
+    let returnValue = '';
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        returnValue = 'Scheme Connection';
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        returnValue = 'Switch Destination';
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        returnValue = 'Transaction Details';
+        break;
+      case 'GET_BIN_DETAILS':
+        returnValue = 'View BIN table defined in the system, Add new BIN and its atributes';
+        break;
+      case 'GET_USERS':
+        returnValue = 'View all the Active users, Create new users, assign roles and permissions.';
+        break;
+      case 'GET_ROUTING_RULES':
+        returnValue = 'Routing Table';
+        break;
+      case 'GET_ROLES':
+        returnValue = 'View all the Active users, Create new users, assign roles and permissions.';
+        break;
+      case 'GET_PRIVILEGES':
+        returnValue = 'View all the Active users, Create new users, assign roles and permissions.';
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        returnValue = 'Service Directory';
+        break;
+      case 'GET_MAKER_CHECKER':
+        returnValue = 'Maker Checker';
+        break;
+      default:
+        break;
+    }
+    return returnValue;
+  }
+
+  public getColumnName(tableID: string): { displayedColumns: string[], columnNames: { [key: string]: string } } {
+    let columnNames: { [key: string]: string } = {};
+    let displayedColumns: string[] = [];
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        columnNames = {
+          destination: 'Destination',
+          connectionId: 'Connection ID',
+          connectionPoolId: 'Connection Pool ID',
+          ip: 'IP',
+          port: 'Port',
+          mode: '	Mode',
+          action: 'Actions',
+        };
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        columnNames = {
+          destination: 'Destination',
+          name: 'Name',
+          destinationId: 'Destination ID',
+          action: 'Actions',
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_ROUTING_RULES':
+        columnNames = {
+          source: 'Source',
+          transactionType: 'Transaction Type',
+          onUsOffUs: 'Onus / Offus',
+          cardBrand: 'CardBrand',
+          destination: '	Destination',
+          destinationId: 'Destination ID',
+          action: 'Actions',
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        columnNames = {
+          transactionDate: ' Transaction Date',
+          postingDate: ' Posting Date',
+          merchantNo: ' Merchant No',
+          amount: 'Amount',
+          rrn: 'RRN',
+          terminal: 'Terminal',
+          transactionType: 'Transaction Type',
+          resp: ' Resp',
+          cardNo: 'Card No',
+          startDateAndTime: 'Start Date And Time',
+          effTime: 'Eff.Time'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_BIN_DETAILS':
+        columnNames = {
+          binNumber: 'Bin Number',
+          countryCode: 'Country Code',
+          countryCurrency: 'Country Currency',
+          productId: 'Product Id',
+          bankName: ' Bank Name',
+          cardName: 'Card Name',
+          cardType: 'Card Type'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_USERS':
+        columnNames = {
+          fullName: 'Full Name',
+          userId: 'User Id',
+          email: 'Email Id',
+          status: 'Status',
+          action: 'Actions'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_ROLES':
+        columnNames = {
+          roleTitle: ' Role Title',
+          description: 'Description',
+          action: 'Actions'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_PRIVILEGES':
+        columnNames = {
+          privilegeNumber: 'Privilege Number',
+          privilegeType: 'Privilege Type',
+          userDescription: 'userDescription',
+          action: 'Actions'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        columnNames = {
+          name: 'Name',
+          providerId: 'Provider Id',
+          serviceName: 'Service Name',
+          action: 'Actions'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      case 'GET_MAKER_CHECKER':
+        columnNames = {
+          makerName: 'Maker Name',
+          checkerName: 'Checker Name',
+          modelName: 'Model Name',
+          entryDateTime: 'Entry Date Time',
+          expiryDateTime: 'Expiry Date Time',
+          status: 'Status',
+          action: 'Actions'
+        }
+        displayedColumns = Object.keys(columnNames);
+        break;
+      default:
+        break;
+    }
+    return { displayedColumns, columnNames };
+  }
+
+  public getDisplayedColumns(tableID: string) {
+    let columnName!: string[];
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        columnName = ['destination', 'connectionId', ' connectionPoolId', 'ip', ' port', 'mode', 'action'];
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        columnName = ['destination', 'name', 'destinationId', 'action'];
+        break;
+      case 'GET_ROUTING_RULES':
+        columnName = ['source', 'transactionType', 'onUsOffUs', 'cardBrand', 'destination', ' destinationId', 'action'];
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        columnName = ['transactionDate', 'postingDate', ' merchantNo', ' amount', ' rrn', 'terminal', ' transactionType', 'resp', 'cardNo', 'startDateAndTime', ' effTime'];
+        break;
+      case 'GET_BIN_DETAILS':
+        columnName = [' binNumber', 'countryCode', 'countryCurrency', ' productId', 'bankName', 'cardName', ' cardType'];
+        break;
+      case 'GET_USERS':
+        columnName = ['fullName', 'userId', ' email', ' status',];
+        break;
+      case 'GET_ROLES':
+        columnName = ['roleTitle', 'description'];
+        break;
+      case 'GET_PRIVILEGES':
+        columnName = ['privilegeNumber', 'privilegeType', 'userDescription', 'action'];
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        columnName = ['name', 'providerId', 'serviceName', 'action'];
+        break;
+      case 'GET_MAKER_CHECKER':
+        columnName = ['makerName', 'checkerName', 'modelName', 'entryDateTime', 'expiryDateTime', 'status', 'action'];
+        break;
+      default:
+        break;
+    }
+    return columnName;
+  }
+
+  public getAPIPATH(tableID: string) {
+    this.searchText = '';
+    let columnName: APIPath;
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_ROUTING_RULES':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_BIN_DETAILS':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_USERS':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_ROLES':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_PRIVILEGES':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      case 'GET_MAKER_CHECKER':
+        // columnName = APIPath.SW_DESTINATION_GETALL;
+        break;
+      default:
+        break;
+    }
+    return columnName!;
+  }
+
+
+  public goToCreate(tableID: string) {
+    let creurl: string;
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        creurl = '/switch-config/scheme-connection/';
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        creurl = '/switch-config/switch-destination/';
+        break;
+      case 'GET_ROUTING_RULES':
+        creurl = '/switch-config/routing-rules/';
+        break;
+      default:
+      case 'GET_TRANSACTION_DETAILS':
+        creurl = '/switch-config/txn-details/';
+        break;
+      case 'GET_BIN_DETAILS':
+        creurl = '/switch-config/bin-table/';
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        creurl = '/switch-config/service-directory/';
+        break;
+      case 'GET_USERS':
+        creurl = '/user-access/user/';
+        break;
+      case 'GET_ROLES':
+        creurl = '/user-access/role/';
+        break;
+      case 'GET_PRIVILEGES':
+        creurl = '/user-access/privilege/';
+        break;
+      case 'GET_MAKER_CHECKER':
+        creurl = '/switch-config/maker-checker/';
+        break;
+    }
+    this.router.navigate([creurl!]);
+  }
+  public goToInq(tableID: string, modelKey: string) {
+    let inqurl: string;
+    switch (tableID) {
+      case 'GET_SCHEME_CONNECTION':
+        inqurl = '/switch-config/scheme-connection/' + modelKey;
+        break;
+      case 'GET_SWITCH_DESTINATIONS':
+        inqurl = '/switch-config/switch-destination/' + modelKey;
+        break;
+      case 'GET_ROUTING_RULES':
+        inqurl = '/switch-config/routing-rules/' + modelKey;
+        break;
+      case 'GET_TRANSACTION_DETAILS':
+        inqurl = '/switch-config/txn-details/' + modelKey;
+        break;
+      case 'GET_BIN_DETAILS':
+        inqurl = '/switch-config/bin-table/' + modelKey;
+        break;
+      case 'GET_SERVICE_DIRECTORY':
+        inqurl = '/switch-config/service-directory/' + modelKey;
+        break;
+      case 'GET_USERS':
+        inqurl = '/user-access/user/' + modelKey;
+        break;
+      case 'GET_ROLES':
+        inqurl = '/user-access/role/' + modelKey;
+        break;
+      case 'GET_PRIVILEGES':
+        inqurl = '/user-access/privilege/' + modelKey;
+        break;
+      case 'GET_MAKER_CHECKER':
+        inqurl = '/switch-config/maker-checker/' + modelKey;
+        break;
+      default:
+        break;
+    }
+    this.router.navigateByUrl(inqurl!);
+  }
 }
